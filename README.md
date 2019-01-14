b53f16549ba7e303a1db7edfd0cf34a21897414f
hq <sectumsempra@yeah.net>
2019-01-14 22:50:26 +0800

    clean

---
+++ README.md
@@ -0,0 +1,13 @@
+# dotfiles
+backup some linux dotfiles
+- zsh
+- vim
+- pip
+- icons & themes
+
+## some other resources:
+1. proxychains: https://github.com/haad/proxychains
+2. zsh:https://github.com/robbyrussell/oh-my-zsh
+
+## Reference:
+https://github.com/zingdle/dotfiles
---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# helper
+must() {
+  if !(command -v "$1" >/dev/null 2>&1); then
+    echo "$1 not installed, exiting..."
+    exit 1
+  fi
+}
+
+must git
+
+for config in **/config.sh; do
+  echo running ${config}
+  DIR=$(dirname "${config}") source ${config}
+done
